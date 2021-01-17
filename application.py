dc709c14846dbadbe2a8b541509815560a0e3162
NeilSinclair <neil.ian.sinclair@gmail.com>
2021-01-17 20:34:04 +0200

    Application.py update

--- application.py
+++ application.py
@@ -28,26 +28,74 @@ node_trace, edge_trace = create_graph_display(G, pos)
 
 ## Layout comprises two tabs - one for viewing of the graph and the other for making a shopping list
 # Get the name of the tabs, and their associated id
-dict_tabs={
-    "Network Graph":"tab1",
-    "Shopping List":"tab2"
-}
 
 app.layout = html.Div([
-    html.H1('Grocery Graph Network'),
-    dcc.Graph(id='graph-graphic'),
-
-    dcc.Slider(
-        id='min-edges-slider',
-        min=5,
-        max=20,
-        value=10,
-        step=None,
-        # marks = {1:'1 edge', 2:'2 edges', 3:'3 edges', 4:'4 edges', 5: '5 edges',
-        #          6:'6 edge', 7:'7 edges', 8:'8 edges', 9:'9 edges', 10: '10 edges'}
-        marks = {5:'5 edges', 10:'10 edges', 15:'15 edges', 20:'20 edges', 25: '25 edges'}
-    )
+    dcc.Tabs(id='tabs-example', value='tab-1', children=[
+        dcc.Tab(label='Tab one', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+    html.Div(id='tabs-example-content')
 ])
+
+@app.callback(Output('tabs-example-content', 'children'),
+              Input('tabs-example', 'value'))
+def render_content(value):
+    if value == 'tab-1':
+        return display_network_graph()
+    elif value == 'tab-2':
+        return display_shopping_list()
+
+
+# app.layout = html.Div([
+#     html.H1('Grocery Graph Network'),
+#     html.Div(
+#         children=[
+#                 dcc.Tabs(id='tabs', value='tab2', children=[
+#                     dcc.Tab(label='Network Graph', value='tab1'),
+#                     dcc.Tab(label='Shopping List Creator', value='tab2')
+#                 ]
+#             )
+#         ],
+#         ),
+#     # This is the output of the tab - i.e. the page selected
+#     html.Div(id='tab_output')
+# ])
+#
+# @app.callback(dash.dependencies.Output('tab_output', 'children'),
+#               dash.dependencies.Input('tabs', 'value'))
+# def display_tab(value):
+#     # print(value)
+#     if value == 'tab1':
+#         return display_network_graph()
+#     else:
+#         return display_shopping_list()
+
+# def display_network_graph():
+#     ''' Function that displays the network tab'''
+#     graph = dcc.Graph(id='graph-graphic'),
+#
+#     slider = dcc.Slider(
+#         id='min-edges-slider',
+#         min=5,
+#         max=20,
+#         value=10,
+#         step=None,
+#         # marks = {1:'1 edge', 2:'2 edges', 3:'3 edges', 4:'4 edges', 5: '5 edges',
+#         #          6:'6 edge', 7:'7 edges', 8:'8 edges', 9:'9 edges', 10: '10 edges'}
+#         marks = {5:'5 edges', 10:'10 edges', 15:'15 edges', 20:'20 edges', 25: '25 edges'}
+#     )
+#     return html.Div(children=[graph, slider])
+
+def display_shopping_list():
+    return html.Div(children=[
+        html.H3('Tab content 2')
+    ])
+
+def display_network_graph():
+    return html.Div(children=[
+        html.H3('Tab content 1')
+    ])
+
 
 @app.callback(
     Output('graph-graphic', 'figure'),
